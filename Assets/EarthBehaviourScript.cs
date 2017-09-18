320e18e6994ed57e644278c37630e25c7135dc7d
miaorenjie <501216475@qq.com>
2017-09-18 22:30:58 +0800

    地球跟地名还有头像的雏形完成

---
+++ Assets/EarthBehaviourScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthBehaviourScript : MonoBehaviour {
+   
+    private float speed=3f, tempSpeed, cXY;
+    public GameObject earth;
+    public GameObject text;
+    private bool onDrag=false;
+    private float axisX;
+    private float axisY;
+    private float oldRotate;
+    private float newRotate;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        axisX = 0;
+        axisY = 0;
+        oldRotate = 0;
+        Debug.Log("点击了球");
+    }
+    private void OnMouseDrag()
+    {
+                
+        axisX = -Input.GetAxis("Mouse X");
+        axisY = Input.GetAxis("Mouse Y");
+        //Debug.Log(axisX + "--" + axisY);
+        System.Console.WriteLine(axisX+"--"+axisY);
+        cXY = Mathf.Sqrt(axisX * axisX + axisY * axisY);
+        onDrag = true;
+    }
+
+    float Rigid()
+    {
+        if (onDrag)
+        {
+            tempSpeed = speed;
+        }
+        else
+        {
+            if (tempSpeed > 0)
+            {
+                //通过除以鼠标移动长度实现拖拽越长速度减缓越慢    
+                if (cXY != 0)
+                {
+                    tempSpeed -= speed * 2 * Time.deltaTime / cXY;
+                }
+            }
+            else
+            {
+                tempSpeed = 0;
+            }
+        }
+        return tempSpeed;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        float rigidSpeed = Rigid();
+        gameObject.transform.Rotate(new Vector3(axisY,axisX,0)* rigidSpeed, Space.World);
+        if (rigidSpeed >= 0)
+        {
+            text.SendMessage("getMessage", gameObject.transform.rotation);
+        }
+        if (!Input.GetMouseButton(0))
+        {
+            onDrag = false;
+        }
+       
+    }
+}
---
+++ Assets/TextBehaviorScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBehaviorScript : MonoBehaviour {
+    // Use this for initialization
+    public GameObject text;
+    public GameObject ball;
+    private float axisY;
+    private float axisX;
+    private float rigidSpeed;
+    private Quaternion quaternion;
+    void Start () {
+       // Debug.Log(gameObject.name);
+
+    }
+    void getMessage(Quaternion str)
+    {
+        quaternion = str;
+        //string[] strs = str.Split(' ');
+        //if (float.Parse(strs[0]) <= 0)
+        //    rigidSpeed = 0;
+        //else
+        //    rigidSpeed=float.Parse(strs[0]);
+        //axisX = float.Parse(strs[1]);
+        //axisY = float.Parse(strs[2]);
+        //Debug.Log("text:"+ rigidSpeed);
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("点击了Text");
+    }
+    // Update is called once per frame
+    void Update () {
+        //text.transform.RotateAround(ball.transform.localPosition, new Vector3(axisY, axisX, 0), 180*Time.deltaTime);
+        text.transform.rotation = quaternion;
+        //GameObject gameObject = GameObject.Find("Sphere");
+        //EarthBehaviourScript earthScript = gameObject.GetComponent<EarthBehaviourScript>();
+        //float i = earthScript.speed;
+        //Debug.Log(i);
+    }
+}
